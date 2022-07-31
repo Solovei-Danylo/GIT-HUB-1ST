b68750b086d702a3173c3b8f09c0a5bcc8b53bec
Solovei-Danylo <trademort@gmail.com>
2022-07-31 22:00:57 +0300

    sss

---
+++ Python/Test.py
@@ -0,0 +1,4 @@
+x = 25
+y = 26
+result = f"{x}+{y}={x+y}"
+print(result)
---
+++ Python/Test2.py
@@ -0,0 +1,8 @@
+from traceback import print_tb
+
+
+number = input("Enter nubmer: ")
+number = float(number)
+
+qube_root = number ** (1/3)
+print(f"Qube root from {number} is {qube_root}")
