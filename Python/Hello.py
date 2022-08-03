bf29b1f4a890870d8b4112540661e4a34524dccd
Solovei-Danylo <trademort@gmail.com>
2022-08-03 19:58:10 +0300

    tt

--- Python/Hello.py
+++ Python/Hello.py
@@ -1,3 +1,7 @@
+# print / dir / help /int / float /bool /
+# bool / Nonetype / str / input
+
+input("Enter your email:")
 print("Hello world!")
 print("Lets go to do somes!!")
 print("Lets do it ")
--- Python/Test.py
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-x = 1
-y = 2
-=======
-x = 25
-y = 26
-result = f"{x}+{y}={x+y}"
-print(result)
->>>>>>> b68750b086d702a3173c3b8f09c0a5bcc8b53bec
