aaa0bff37a9a04f7dc01f272ee60def7997bb36d
Solovei-Danylo <trademort@gmail.com>
2022-08-07 17:53:59 +0300

    22

---
+++ Python/test10.py
@@ -0,0 +1,7 @@
+x = int(input("X: "))
+y = int(input("Y: "))
+while True:
+    user_input = input()
+    print(user_input)
+    if user_input == "exit":
+        break
---
+++ Python/test7.py
@@ -0,0 +1,23 @@
+
+DEFAULT_DOMAIN = "goit.ua"
+
+email = input("Enter your email: ")
+phone = input("Enter yourphone number: ")
+
+# "serg@gmail.com"
+# "sergdsds.sdsdsds.sdsd@gmail.com"
+
+index = email.index('@')
+
+nickname = email[:index]
+
+domain = email[index+1:]
+new_email = nickname + '@' + DEFAULT_DOMAIN
+
+print('Нік: ', nickname)
+print('Домен: ', domain)
+print('Домен: ', domain)
+print('Новий email: ', new_email)
+
+
+# м.Київ, проспект Степана Бандери 20
