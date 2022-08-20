e9951f1a3ff2116e6ff92800484a7aa5da8442f9
Solovei-Danylo <trademort@gmail.com>
2022-08-20 20:59:32 +0300

    11

---
+++ Python/test25.py
@@ -0,0 +1,8 @@
+
+
+def invite_to_event(username=input("Your name? :")):
+    return (f"Dear {username}, we have the honour to invite you to our event")
+
+
+invite_to_event()
+print(invite_to_event)
