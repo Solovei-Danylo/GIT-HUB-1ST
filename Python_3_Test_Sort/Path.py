05590c7f0eca18f34124f1af89ac11ea58db528b
Solovei-Danylo <trademort@gmail.com>
2022-09-18 19:40:48 +0300

    123

--- Python_3_Test_Sort/Path.py
+++ Python_3_Test_Sort/Path.py
@@ -1,7 +1,18 @@
-from pathlib import *
+import pathlib
 
-PathExample = Path('Timeweb', 'Cloud', 'Pathlib')
-PathExample = Path.home() / PathExample
-print(PathExample)
-AttributeExample = PathExample / 'file.txt'
-print(AttributeExample)
+
+def get_all_files(dir_path_to_search):
+    filename_list = []
+
+    file_iterator = dir_path_to_search.iterdir()
+
+    for entry in file_iterator:
+        if entry.is_file():
+            # print(entry.name)
+            filename_list.append(entry.name)
+
+    return filename_list
+
+
+dir_path_to_search = pathlib.Path("D:\Загрузки с браузера")
+print(get_all_files(dir_path_to_search))
